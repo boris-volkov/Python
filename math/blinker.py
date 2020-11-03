cb6596f606ccb4667a8fd1c766e1778827b48662
boris-volkov <boris366@gmail.com>
2020-11-02 20:51:34 -0800

    blinker

--- math/blinker.py
+++ math/blinker.py
@@ -1,6 +1,6 @@
-import sounds
 import colors
 import time
+import sys
 
 dot = '◉'
 black = colors.rgb(0,0,0)
@@ -8,11 +8,12 @@ red = colors.yellow
 yellow = colors.yellow
 margin = ' '*6
 
-def grid(width = 10, counter = 0,rate = 1):
+def grid(width = 10, counter = 0,rate = 0.05):
     print(colors.hide_cursor)
     while 1:
         print('\n' + margin, end = '')
         for i in range(1,width**2+1):
+            print('', end = ' ')
             if counter % i == 0 and i%width == 0:
                 print(red + dot)
                 print(margin, end = '')
@@ -25,4 +26,9 @@ def grid(width = 10, counter = 0,rate = 1):
                 print(black + dot, end = '')
         print(yellow + str(counter))
         time.sleep(rate)
+        print(colors.clear_screen)
         counter += 1
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        grid(int(sys.argv[1]))
